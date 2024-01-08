94eac9950d30adecd965b209c66ad98b55ef00cd
edwin <edwinswatpako@gmail.com>
2024-01-08 15:27:56 +0700

    rmq: dummy integration

--- indexer/src/errors.rs
+++ indexer/src/errors.rs
@@ -6,6 +6,12 @@ pub enum Error {
     IoError(#[from] std::io::Error),
     #[error("anyhow error")]
     AnyhowError(#[from] anyhow::Error),
+    #[error(transparent)]
+    LapinError(#[from] lapin::Error),
+    #[error(transparent)]
+    LapinBuildError(#[from] deadpool::managed::BuildError),
+    #[error("rmq pool error: {0}")]
+    RMQPoolError(#[from] deadpool_lapin::PoolError),
 }
 
 pub type Result<T, E = Error> = std::result::Result<T, E>;
